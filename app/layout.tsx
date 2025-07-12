ec414411fc18e52011ce861636ff05360d6bc458
Anurag113-sonu <k.anuragsahoo@gmail.com>
2025-07-12 23:00:47 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Anurag AI',
+  description:
+    'Anurag AI is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+  generator: 'Rohan Sharma',
+  icons: {
+    icon: '/logo.png',
+  },
+  openGraph: {
+    title: 'Anurag AI',
+    siteName: 'Anurag-ai',
+    url: 'https://intello-ai.vercel.app/',
+    description:
+      'Radhika is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Radhika AI Chatbot',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Anurag AI ',
+    description:
+      'Anurag AI  is a versatile AI chatbot designed to assist with a wide range of tasks, from answering questions to providing recommendations and engaging in casual conversation.',
+    images: ['/og-image.jpg'],
+  },
+}
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
